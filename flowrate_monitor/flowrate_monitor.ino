6b3d8b63bc99b028636b78a8183d1eda2e41233f
rbmasek <r.masek97@gmail.com>
2018-02-13 00:53:13 -0600

    Added comments and organized code. Renamed file to proper name.

--- flowrate_monitor/flowrate_monitor.ino
+++ flowrate_monitor/flowrate_monitor.ino
@@ -2,16 +2,19 @@
 #include <LiquidCrystal.h>
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
+//Define pins and variables
 const int flowPin = A0;
 const String unit = " mL/min";
 float flowRate = 0;
 String previousRate;
 
+//Returns flowrate value that will be filtered through SMA
 float getFlowRate()
 {
   return mapf(analogRead(flowPin), 0, 1023, 0, 50);
 }
 
+//Redefines map() function to report fractions
 float mapf(float x, float in_min, float in_max, float out_min, float out_max)
 {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
@@ -22,6 +25,7 @@ SMAFilter sma0(2, getFlowRate);
 void setup() {
   Serial.begin(9600);
 
+  //Initialize LCD
   lcd.begin(16,2);
   lcd.print("Flowrate");
   lcd.setCursor(0,1);
@@ -31,15 +35,18 @@ void setup() {
 }
 
 void loop() {
+  //LCD Output
   lcd.setCursor(0,0);
-  lcd.print("Prev:"+previousRate);
+  lcd.print("Prev:" + previousRate);
   lcd.setCursor(0,1);
   flowRate =  sma0.getFilteredSample();
-  lcd.print("Curr:"+(flowRate + unit));
-  lcd.setCursor(0,1);
+  lcd.print("Curr:" + (flowRate + unit));
+
+  //Serial Monitor output
   Serial.print("Flow Rate: ");
   Serial.print(flowRate);
-  Serial.println(" mL/min");
+  Serial.println(unit);
+  
   previousRate = flowRate + unit;
   delay(500);
 }
